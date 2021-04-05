b580d7a4b3236a8fc46a1f553c5c25a2cfb92be8
GrpeApple <grapeapplemelonwater@gmail.com>
2021-04-05 19:02:07 +0800

    Use options

--- src/tarmux.sh
+++ src/tarmux.sh
@@ -1,3 +1,48 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 
-echo 'Hello, World!'
+opt="$(getopt --options 'hV' --alternative --longoptions 'help,version' --name 'tarmux' --shell 'bash' -- "${@}")"
+
+options () {
+	while true; do
+		case "${1}" in
+			' '|'-h'|'--help')
+				usage
+				break
+				;;
+
+			'-V'|'--version')
+				version
+				shift 1
+				continue
+				;;
+
+			'--')
+				break
+				;;
+
+			*)
+				echo 'Unknown error' >&2
+				exit 1
+				break
+				;;
+		esac
+	done
+}
+
+usage () {
+cat <<EOU
+Usage: $(basename "${0}") -[[h|-help]|[v|-version]]
+Options:
+	-h	Show this help usage
+	-V	Display version and information
+EOU
+}
+
+version () {
+	cat <<-EOV
+		Tarmux v0.0.3
+	EOV
+}
+
+
+options ${opt}
