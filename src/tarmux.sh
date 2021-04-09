fdc9b190b23751c738b6c9d375f23c831a01abec
GrpeApple <grapeapplemelonwater@gmail.com>
2021-04-09 19:41:28 +0800

    All-in-one script

--- src/tarmux.sh
+++ src/tarmux.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 
-opt="$(getopt --options 'hV' --alternative --longoptions 'help,version' --name 'tarmux' --shell 'bash' -- "${@}")"
+opt="$(getopt --options 'hcV' --alternative --longoptions 'help,configure,version' --name 'tarmux' --shell 'bash' -- "${@}")"
 
 options () {
 	while true; do
@@ -8,6 +8,12 @@ options () {
 			'-h'|'--help')
 				usage
 				break
+				;;
+
+			'-c'|'--configure')
+				configure
+				shift 1
+				continue
 				;;
 
 			'-V'|'--version')
@@ -34,8 +40,86 @@ cat <<EOU
 Usage: $(basename "${0}") -[[h|[-]help]|[v|[-]version]]
 Options:
 	-h|-help	Display this help usage
+	-c|-configure	Configure
 	-V|-version	Display version and information
 EOU
+}
+
+configure () {
+	installPkg () {
+		select compression in 'update' 'upgrade' 'tar (default)' 'tar (pigz)' 'tar (zstd)' 'clear' 'exit'; do
+			case "${compression},${REPLY}" in
+				'update',*|*,'update')
+					echo 'Updating apt'
+					apt update
+					;;
+				'upgrade',*|*,'upgrade')
+					echo 'Upgrading packages'
+					apt full-upgrade -y
+					;;
+				'tar (default)',*|*,'tar (default)')
+					echo 'Installing tar (default)'
+					apt install tar
+					;;
+				'tar (pigz)',*|*,'tar (pigz)')
+					echo 'Installing tar (pigz)'
+					apt install tar pigz
+					;;
+				'tar (zstd)',*|*,'tar (zstd)')
+					echo 'Installing tar (zstd)'
+					apt install tar zstd
+					;;
+				'clear',*|*,'clear')
+					clear
+					;;
+				'exit',*|*,'exit')
+					echo 'Exiting installation'
+					break
+					;;
+				*)
+					echo "Unknown option"
+					;;
+			esac
+		done
+	}
+	uninstallPkg () {
+		select compression in 'tar (default)' 'tar (pigz)' 'tar (zstd)' 'clear' 'exit'; do
+			case "${compression},${REPLY}" in
+				'tar (default)',*|*,'tar (default)')
+					echo 'Uninstalling tar (default)'
+					apt autoremove tar
+					;;
+				'tar (pigz)',*|*,'tar (pigz)')
+					echo 'Uninstalling tar (pigz)'
+					apt autoremove tar pigz
+					;;
+				'tar (zstd)',*|*,'tar (zstd)')
+					echo 'Uninstalling tar (zstd)'
+					apt autoremove tar zstd
+					;;
+				'clear',*|*,'clear')
+					clear
+					;;
+				'exit',*|*,'exit')
+					echo 'Exiting uninstallation'
+					break
+					;;
+				*)
+					echo "Unknown option"
+					;;
+			esac
+		done
+	}
+	select choice in 'install' 'uninstall' 'exit'; do
+		case "${choice},${REPLY}" in
+			'install',*|*,'install') installPkg;;
+			'uninstall',*|*,'uninstall') uninstallPkg;;
+			'exit',*|*,'exit')
+				echo 'Exiting'
+				break
+				;;
+		esac
+	done
 }
 
 version () {
