e9ce38e0e2925a07a1636b3215eb100d9fa59978
GrpeApple <grapeapplemelonwater@gmail.com>
2021-04-10 18:49:46 +0800

    Insane improvements on tarmux; fix documentation; add configuration; version; colors; selection; options; and even more.

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Tarmux
-Backup files on Termux with tar
+Backup files on Termux with `tar`
 
 ---
 
 ## Installation
 
+----
 ### Install `make`
-
-
 
 ```bash
 apt update
-apt full-upgrade -y
-apt install make -y
+apt full-upgrade
+apt install make
 ```
 
+-----
 ### Installing
 
 ```bash
@@ -23,15 +23,13 @@ make install
 tarmux -c # Select installation
 ```
 
-
-
+----
 ## Uninstallation
 
 You must install [`make`](#install-make)
 
+-----
 ### Uninstall
-
-
 
 ```bash
 tarmux -c # Select uninstallation
@@ -39,21 +37,28 @@ make clean
 make uninstall
 ```
 
-
+-----
 ### Uninstall `make`
-
-
 
 ```bash
 apt autoremove make
 ```
 
 ----
-
 ## Usage
+
+-----
+### Configuration
+
+There is a config (`CONFIG_DIR` (default: `/data/data/com.termux/files/home/.config/tarmux`)/`CONFIG_FILE` (default: `config`)) file (default: `/data/data/com.termux/files/home/.config/tarmux`).
+You are responsible for your actions of adding dangerous stuff to your config file. (It uses `source` so it will run commands like `rm -rf *` if added to the config file)
+
+-----
+### Options
 
 You can use `-some-long-option` or `-some-long` and it will still work.
 
+------
 <table>
 <thead>
 	<tr>
--- src/tarmux.sh
+++ src/tarmux.sh
@@ -1,13 +1,61 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 
-opt="$(getopt --options 'hcV' --alternative --longoptions 'help,configure,version' --name 'tarmux' --shell 'bash' -- "${@}")"
+VERSION='v0.1.0'
+
+declare -A color=(
+	['RED']='\e[0;31m'
+	['BRED']='\e[1;31m'
+	['BGREEN']='\e[1;32m'
+	['BYELLOW']='\e[1;33m'
+	['BBLUE']='\e[1;34m'
+	['BPURPLE']='\e[1;35m'
+	['BCYAN']='\e[1;36m'
+	['BWHITE']='\e[1;37m'
+	['KBLACK']='\e[40m'
+	['RESET']='\e[0m'
+)
+declare -A config=(
+	['INSTALL']="$(realpath "${0:-tarmux}")"
+)
+
+CONFIG_DIR="${HOME:-'/data/data/com.termux/files/home'}/.config/tarmux"
+CONFIG_FILE="config"
+
+mkdir --parents "${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}"
+touch "${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}/${CONFIG_FILE:-'config'}"
+
+INSTALL="${config['INSTALL']}"
+
+# You are responsible for putting dangerous stuff in the config file.
+source "${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}/${CONFIG_FILE:-'config'}"
+
+if [[ "${INSTALL}" != "${config['INSTALL']}" ]]; then
+	readarray warning <<-EOW
+		WARNING: moving ${INSTALL} to ${config['INSTALL']}.
+		Use ${0:-tarmux} -c and select 'Installation directory' to move; or use ${0:-tarmux} -c and select 'reset' then move it to the desired location.
+	EOW
+	printf "${color['BYELLOW']}%s${color['RESET']}" "${warning[@]}" >&2
+	mv --interactive --no-target-directory "${INSTALL}" "${config['INSTALL']}"
+fi
+
+opt="$(getopt --options 'hcV' --alternative --longoptions 'help,configure,version' --name 'tarmux' --shell 'bash' -- "${@:---}")"
+
+ACTIONS=('install' 'uninstall' 'tarmux')
+PACKAGES=('tar' 'pigz' 'zstd')
+CONFIGURATIONS=('Installation directory' 'reset')
+
+save_config () {
+	cat > "${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}/${CONFIG_FILE:-'config'}" <<-EOC
+		$(for key in "${!config[@]}"; do echo "config['${key}']='${config[${key}]}'"; done)
+	EOC
+}
 
 options () {
 	while true; do
-		case "${1}" in
+		case "${1:---}" in
 			'-h'|'--help')
 				usage
-				break
+				break 1
 				;;
 
 			'-c'|'--configure')
@@ -23,110 +71,181 @@ options () {
 				;;
 
 			'--')
-				break
+				break 1
 				;;
 
-			*)
-				echo 'Unknown error' >&2
-				exit 1
-				break
-				;;
+			*) printf "${color['BRED']}%s\n${color['RESET']}" 'Unknown error' >&2; exit 1; break 1;;
 		esac
 	done
 }
 
 usage () {
-cat <<EOU
-Usage: $(basename "${0}") -[[h|[-]help]|[c|[-]configure]|[v|[-]version]]
-Options:
+	printf "${color['BCYAN']}%s\n${color['RESET']}" "Usage: $(basename "${config['INSTALL']}") -[[h|[-]help]|[c|[-]configure]|[v|[-]version]]"
+	printf "${color['BBLUE']}%s\n${color['RESET']}" 'Options:'
+readarray options <<EOU
 	-h|-help	Display this help usage
 	-c|-configure	Configure
 	-V|-version	Display version and information
 EOU
+	printf "${color['BWHITE']}%s${color['RESET']}" "${options[@]}"
 }
 
 configure () {
 	installPkg () {
-		select compression in 'update' 'upgrade' 'tar (default)' 'tar (pigz)' 'tar (zstd)' 'clear' 'exit'; do
-			case "${compression},${REPLY}" in
-				'update',*|*,'update')
-					echo 'Updating apt'
-					apt update
-					;;
-				'upgrade',*|*,'upgrade')
-					echo 'Upgrading packages'
-					apt full-upgrade -y
-					;;
-				'tar (default)',*|*,'tar (default)')
-					echo 'Installing tar (default)'
-					apt install tar
-					;;
-				'tar (pigz)',*|*,'tar (pigz)')
-					echo 'Installing tar (pigz)'
-					apt install tar pigz
-					;;
-				'tar (zstd)',*|*,'tar (zstd)')
-					echo 'Installing tar (zstd)'
-					apt install tar zstd
-					;;
-				'clear',*|*,'clear')
-					clear
-					;;
-				'exit',*|*,'exit')
-					echo 'Exiting installation'
-					break
-					;;
-				*)
-					echo "Unknown option"
-					;;
-			esac
+		while true; do
+			select package in 'update' 'upgrade' "${PACKAGES[@]}" 'clear' 'exit'; do
+				case "${package:-'exit'},${REPLY:-'exit'}" in
+					'update',*|*,'update')
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Updating apt'
+						apt update &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'upgrade',*|*,'upgrade')
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Upgrading packages'
+						apt full-upgrade &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'tar'*,*|*,'tar'*)
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Installing tar'
+						apt install tar &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'pigz',*|*,'pigz')
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Installing pigz'
+						apt install pigz &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'zstd',*|*,'zstd')
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Installing zstd'
+						apt install zstd &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'clear',*|*,'clear') clear; break 1;;
+					'exit',*|*,'exit') printf "${color['BRED']}%s\n${color['RESET']}" 'Exiting installation...'; break 2;;
+					*) printf "${color['BRED']}%s\n${color['RESET']}" 'Unknown option' >&2; break 1;;
+				esac
+			done
 		done
 	}
 	uninstallPkg () {
-		select compression in 'tar (default)' 'tar (pigz)' 'tar (zstd)' 'clear' 'exit'; do
-			case "${compression},${REPLY}" in
-				'tar (default)',*|*,'tar (default)')
-					echo 'Uninstalling tar (default)'
-					apt autoremove tar
-					;;
-				'tar (pigz)',*|*,'tar (pigz)')
-					echo 'Uninstalling tar (pigz)'
-					apt autoremove tar pigz
-					;;
-				'tar (zstd)',*|*,'tar (zstd)')
-					echo 'Uninstalling tar (zstd)'
-					apt autoremove tar zstd
-					;;
-				'clear',*|*,'clear')
-					clear
-					;;
-				'exit',*|*,'exit')
-					echo 'Exiting uninstallation'
-					break
-					;;
-				*)
-					echo "Unknown option"
-					;;
-			esac
+		while true; do
+			select package in "${PACKAGES[@]}" 'clear' 'exit'; do
+				case "${package:-'exit'},${REPLY:-'exit'}" in
+					'tar'*,*|*,'tar'*)
+						printf "${color['BRED']}%s\n${color['RESET']}" 'Uninstalling tar (dangerous)'
+						apt autoremove tar &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'pigz',*|*,'pigz')
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Uninstalling pigz'
+						apt autoremove pigz &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'zstd',*|*,'zstd')
+						printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Uninstalling zstd'
+						apt autoremove zstd &&
+						printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+						break 1
+						;;
+
+					'clear',*|*,'clear') clear; break 1;;
+					'exit',*|*,'exit') printf "${color['BRED']}%s\n${color['RESET']}" 'Exiting uninstallation...'; break 2;;
+					*) printf "${color['BRED']}%s\n${color['RESET']}" 'Unknown option' >&2; break 1;;
+				esac
+			done
 		done
 	}
-	select choice in 'install' 'uninstall' 'exit'; do
-		case "${choice},${REPLY}" in
-			'install',*|*,'install') installPkg;;
-			'uninstall',*|*,'uninstall') uninstallPkg;;
-			'exit',*|*,'exit')
-				echo 'Exiting'
-				break
-				;;
-		esac
+	tarmuxConf () {
+		while true; do
+			select configuration in "${CONFIGURATIONS[@]}" 'clear' 'exit'; do
+				case "${configuration:-'exit'},${REPLY:-'exit'}" in
+					'Installation directory',*|*,'Installation directory')
+						while true; do
+							printf "${color['BWHITE']}${color['KBLACK']}%s${color['RESET']}" "Are you sure to move ${config['INSTALL']} to another location? (y/N): "
+							read -r -e # No -n1 because I am not that evil.
+							case "${REPLY:-'n'}" in
+								'y'|'Y')
+									printf "${color['BGREEN']}%s\n${color['RESET']}" 'Continuing...'
+									printf "${color['BWHITE']}${color['KBLACK']}%s${color['RESET']}" 'Where? (): '
+									read -r -e INSTALL
+									printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" "Moving ${config['INSTALL']} to ${INSTALL:-"${config['INSTALL']}"}..."
+									mv --interactive --no-target-directory "${config['INSTALL']}" "${INSTALL:-"${config['INSTALL']}"}" || exit 1
+									config['INSTALL']="${INSTALL:-"${config['INSTALL']}"}"
+									save_config &&
+									printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+									break 1
+									;;
+
+								'n'|'N'|*)
+									printf "${color['BRED']}%s\n${color['RESET']}" 'Exiting installation directiory configuration...'
+									break 1
+									;;
+							esac
+						done
+						break 1
+						;;
+
+					'reset',*|*,'reset')
+						printf "${color['RED']}%s${color['RESET']}" "Are you sure to reset the config from ${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}/${CONFIG_FILE:-'config'}? (y/N): "
+						read -r -e # No -n1 because I am not that evil.
+						case "${REPLY:-'n'}" in
+							'y'|'Y')
+								printf "${color['BWHITE']}${color['KBLACK']}%s\n${color['RESET']}" 'Resetting...'
+								printf '' > "${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}/${CONFIG_FILE:-'config'}" &&
+								printf "${color['BGREEN']}%s\n${color['RESET']}" 'Done!'
+								;;
+
+							'n'|'N'|*)
+								printf "${color['BRED']}%s\n${color['RESET']}" 'Exiting resetting configuration...'
+								;;
+						esac
+						break 1
+						;;
+
+					'clear',*|*,'clear') clear; break 1;;
+					'exit',*|*,'exit') printf "${color['BRED']}%s\n${color['RESET']}" 'Exiting tarmux configuration...'; break 2;;
+					*) printf "${color['BRED']}%s\n${color['RESET']}" 'Unknown option' >&2; break 1;;
+				esac
+			done
+		done
+	}
+	while true; do
+		select action in "${ACTIONS[@]}" 'clear' 'exit'; do
+			case "${action},${REPLY}" in
+				'install',*|*,'install') installPkg; break 1;;
+				'uninstall',*|*,'uninstall') uninstallPkg; break 1;;
+				'tarmux',*|*,'tarmux') tarmuxConf; break 1;;
+				'clear',*|*,'clear') clear; break 1;;
+				'exit',*|*,'exit') printf "${color['BRED']}%s\n${color['RESET']}" 'Exiting...'; break 2;;
+				*) printf "${color['BRED']}%s\n${color['RESET']}" 'Unknown option' >&2; break 1;;
+			esac
+		done
 	done
 }
 
 version () {
-	cat <<-EOV
-		Tarmux v0.0.3.1
-	EOV
+readarray config_variables <<EOV
+	$(for key in "${!config[@]}"; do echo "${key}: '${config[${key}]}'"; done)
+EOV
+	printf "${color['BCYAN']}%s\n${color['RESET']}" "tarmux ${VERSION}" >&2
+	printf "${color['BPURPLE']}%s\n${color['RESET']}" "Config: '${CONFIG_DIR:-'/data/data/com.termux/files/home/.config/tarmux'}/${CONFIG_FILE}'" >&2
+	printf "${color['BWHITE']}%s${color['RESET']}" "${config_variables[@]}" >&2
 }
 
 
-options ${opt}
+save_config
+options ${opt:-'--'} # Without "" is necessary!!!
